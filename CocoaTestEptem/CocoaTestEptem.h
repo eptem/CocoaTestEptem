abd05633d03160b0c6111c653f5f587d847ff68c
eptem <mropp1@mail.ru>
2020-08-05 17:11:32 +0500

    Initial Commit

---
+++ CocoaTestEptem/CocoaTestEptem.h
@@ -0,0 +1,19 @@
+//
+//  CocoaTestEptem.h
+//  CocoaTestEptem
+//
+//  Created by Artem Zhornitsky on 05.08.2020.
+//  Copyright © 2020 eptem. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CocoaTestEptem.
+FOUNDATION_EXPORT double CocoaTestEptemVersionNumber;
+
+//! Project version string for CocoaTestEptem.
+FOUNDATION_EXPORT const unsigned char CocoaTestEptemVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaTestEptem/PublicHeader.h>
+
+
